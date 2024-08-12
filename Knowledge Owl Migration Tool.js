6ebef008798fc4f0291e1cabe3764c0539034c78
dMoreno2 <d.edwardmoreno@gmail.com>
2024-08-12 17:36:14 +1000

    removed unused Get_URL value from config template;
    Added additional text and changed console output div to pre + added create specfiic route for user;
    
    Added export for output function - so can be called inside of KO Tool script.
    Removed unnessecary console log routing.
    Changed the port to be random on start (issue with 'port in use' all the time).
    Added ignore for console logging on the /event url.
    Removed a lot of of res.write from POST calls - not needed.
    Added createSpecific endpoint.
    Optimized the server events write.
    Added server queue push in Output.
    Added formatting;
    
    Added import for the Output function in server script.
    Changed article get to a single function with varrible.
    Changed the ProcessArticle function to take varribles that equate to true/false for readibility.
    Added Cases for specific article changes.
    Adjusted the test case and automatic to be more unique/useful.
    Added formatting;
    
    Changed from adding inner html to textcontent.
    Added an ok check on fetch requests for events. removed the loop on fetch logs and added them to the call for the function. Every time an endpoint gets called it runs the setInterval for the events till the res comes back.
    Added styling;
    
    Changed formatting of doc on style

--- Knowledge Owl Migration Tool.js
+++ Knowledge Owl Migration Tool.js
@@ -1,12 +1,15 @@
 module.exports = { Program_Switch };
+const { Output } = require("./kbServer.js");
 
-
-const fs = require('fs');
-const path = require('path');
-if (!fs.existsSync('logs')) {
-    fs.mkdirSync('logs');
+const fs = require("fs");
+const path = require("path");
+if (!fs.existsSync("logs")) {
+  fs.mkdirSync("logs");
 }
-const logFilePath = path.join(path.join(__dirname, 'logs'), `log-${GetDateTime()}.json`);
+const logFilePath = path.join(
+  path.join(__dirname, "logs"),
+  `log-${GetDateTime()}.json`
+);
 fs.appendFile(logFilePath, `{`, (err) => { });
 
 var configFile;
@@ -19,222 +22,309 @@ var int_Response = [];
 
 let _articleTile = 0; /// ko_Response [0] - Article Title - int_Response [0]
 let _articleBody = 1; /// ko_Response [1] - Article Body  - int_Response [1]
-let _articleID = 2;   /// ko_Response [2] - Article ID    - int_Response [2]
+let _articleID = 2; /// ko_Response [2] - Article ID    - int_Response [2]
 try {
-    fs.readFile("config.json", 'utf8', (error, data) => {
-        configFile = JSON.parse(data);
-        for (let key in configFile) {
-            if (configFile[key] === null) {
-                throw "Check .config file for null values";
-            }
-        }
-    });
-}
-catch (error) {
-    LogInfo(error);
+  fs.readFile("config.json", "utf8", (error, data) => {
+    configFile = JSON.parse(data);
+    for (let key in configFile) {
+      if (configFile[key] === null) {
+        throw "Check .config file for null values";
+      }
+    }
+  });
+} catch (error) {
+  LogInfo(error);
 }
 
 function Program_Switch(switchCase, flag = null) {
+  let update = true;
+  let create = true;
+  switch (switchCase) {
+    case 1:
+      //gets articles
+      GetArticles();
 
-    switch (switchCase) {
-        case 1:
-            //creates and updates
-            _Automatic();
-            break;
-        case 2:
-            //only updates
-            UpdateArticlesOnly();
-            break;
-        case 3:
-            //only creates
-            CreatArticlesOnly();
-            break;
-        case 4:
-            //updates specific article
-            _Automatic(flag)
-            break;
-        case 443:
-            console.log("help me");
-            break;
-        default:
-            _Automatic();
-            break;
+      //updates and creates
+      //ProcessArticles(update, create);
+      break;
+    case 2:
+      //gets articles
+      GetArticles();
+
+      //onlyupdates
+      ProcessArticles(!update, create);
+      break;
+    case 3:
+      //gets articles
+      GetArticles();
+
+      //only creates
+      ProcessArticles(update, !create);
+      break;
+    case 4:
+      //gets specific article
+      GetArticles(1234);
+
+      //only updates 1 article
+      ProcessArticles(!update, create);
+      break;
+    case 5:
+      //gets specific article
+      GetArticles(1234);
+
+      //only creates 1 article
+      ProcessArticles(update, !create);
+      break;
+    case 6:
+      //gets specific article
+      GetArticles(1234);
+
+      //removes article from intercom
+      DeleteSpecificArticle();
+      break;
+    case 443:
+      //this is a test case
+      Output("debug test");
+      break;
+    default:
+      Output("whatever you did, didn't work...");
+      break;
+  }
+}
+function GetArticles(id = null) {
+  Get_KO_Articles(id);
+  //Get_Intercom_Articles(id);
+  Output("execution stopped");
+}
+
+async function Get_KO_Articles(id) {
+  maxPages = 1;
+  pageCount = 1;
+  fs.appendFile(logFilePath, `{ "KO_Items:"`, (err) => { });
+  if (id === null) {
+    const resp = await CallAPI(
+      "Knowledge_Owl",
+      "GET",
+      `${configFile.Knowledge_Owl.Search_URL}?`,
+      `project_id=${configFile.Knowledge_Owl.Project_ID}`,
+      "status=published",
+      `page=${pageCount}`
+    );
+    for (let index = 0; index < resp.length; index++) {
+      ko_Response.push(resp[index]);
+      LogInfo(`"ko-name": "${resp[index][_articleTile]}"`);
     }
-
-}
-
-async function Get_KO_Articles() {
-    maxPages = 1;
-    pageCount = 1;
-    fs.appendFile(logFilePath, `{ "KO_Items:"`, (err) => { });
+  }
+  else {
     do {
-        const resp = await CallAPI("Knowledge_Owl", "GET", `${configFile.Knowledge_Owl.Search_URL}?`, `project_id=${configFile.Knowledge_Owl.Project_ID}`, "status=published", `page=${pageCount}`);
-        for (let index = 0; index < resp.length; index++) {
-            ko_Response.push(resp[index])
-            LogInfo(`"ko-name": "${resp[index][_articleTile]}"`);
-        }
-        setTimeout(function Waitfor() {
-            LogInfo(`"message":"ko-reply ${pageCount}"`);
-        }, 100);
-        pageCount++;
-    } while (pageCount < maxPages + 1)
-    fs.appendFile(logFilePath, `}`, (err) => { });
+      const resp = await CallAPI(
+        "Knowledge_Owl",
+        "GET",
+        `${configFile.Knowledge_Owl.Search_URL}/${id}`,
+        `?project_id=${configFile.Knowledge_Owl.Project_ID}`
+      );
+      for (let index = 0; index < resp.length; index++) {
+        ko_Response.push(resp[index]);
+        LogInfo(`"ko-name": "${resp[index][_articleTile]}"`);
+      }
+      setTimeout(function Waitfor() {
+        LogInfo(`"message":"ko-page ${pageCount}"`);
+      }, 100);
+      pageCount++;
+    } while (pageCount < maxPages + 1);
+  }
+  fs.appendFile(logFilePath, `}`, (err) => { });
 }
-
 async function Get_Intercom_Articles() {
-    maxPages = 1;
-    pageCount = 1;
-    fs.appendFile(logFilePath, `{ "INT_Items:"`, (err) => { });
-    do {
-        const resp = await CallAPI("Intercom", "GET", configFile.Intercom.Create_URL, '?page=' + pageCount);
-        for (let index = 0; index < resp.length; index++) {
-            int_Response.push(resp[index])
-            LogInfo(`"int-name": "${resp[index][_articleTile]}"`);
-        }
-        setTimeout(function Waitfor() {
-            LogInfo(`"message":"Int-reply ${pageCount}"`);
-        }, 100);
-        pageCount++;
-    } while (pageCount < maxPages + 1)
-    fs.appendFile(logFilePath, `}`, (err) => { });
-}
-
-async function ProcessArticles() {
-    for (let ko_article = 0; ko_article < ko_Response.length; ko_article++) {
-        var exists = false;
-        for (let i = 0; i < int_Response.length; i++) {
-            LogInfo(`"Checking for article": "${ko_Response[ko_article][_articleTile]}..."`);
-            if (ko_Response[ko_article][_articleTile].trim() === int_Response[i][_articleTile].trim()) {
-                LogInfo(`"Updating Article": "${ko_Response[ko_article][_articleTile]}"\n`);
-                await UpdateArticle(ko_article, _articleTile, i);
-                exists = true;
-                break;
-            }
-            else {
-                await setTimeout(function Waitfor() { }, 1000);
-            }
-        }
-        if (exists == false) {
-            LogInfo(`Creating article: ${ko_Response[ko_article][_articleTile]}...`);
-            await CreateArticle(ko_article, _articleTile);
-        }
-        else {
-            exists = false;
-        }
-        iterationCount++;
+  maxPages = 1;
+  pageCount = 1;
+  fs.appendFile(logFilePath, `{ "INT_Items:"`, (err) => { });
+  do {
+    const resp = await CallAPI(
+      "Intercom",
+      "GET",
+      configFile.Intercom.Create_URL,
+      "?page=" + pageCount
+    );
+    for (let index = 0; index < resp.length; index++) {
+      int_Response.push(resp[index]);
+      LogInfo(`"int-name": "${resp[index][_articleTile]}"`);
     }
+    setTimeout(function Waitfor() {
+      LogInfo(`"message":"Int-reply ${pageCount}"`);
+    }, 100);
+    pageCount++;
+  } while (pageCount < maxPages + 1);
+  fs.appendFile(logFilePath, `}`, (err) => { });
 }
-
+async function ProcessArticles(create, update) {
+  for (let ko_article = 0; ko_article < ko_Response.length; ko_article++) {
+    var exists = false;
+    for (let i = 0; i < int_Response.length; i++) {
+      LogInfo(
+        `"Checking for article": "${ko_Response[ko_article][_articleTile]}..."`
+      );
+      if (
+        ko_Response[ko_article][_articleTile].trim() ===
+        int_Response[i][_articleTile].trim()
+      ) {
+        if (update === true) {
+          LogInfo(
+            `"Updating Article": "${ko_Response[ko_article][_articleTile]}"\n`
+          );
+          await UpdateArticle(ko_article, _articleTile, i);
+          exists = true;
+          break;
+        }
+      } else {
+        await setTimeout(function Waitfor() { }, 1000);
+      }
+    }
+    if (exists == false && create === true) {
+      LogInfo(`Creating article: ${ko_Response[ko_article][_articleTile]}...`);
+      await CreateArticle(ko_article, _articleTile);
+    } else {
+      exists = false;
+    }
+    iterationCount++;
+  }
+}
 async function UpdateArticle(ko_article, _articleTile, i) {
-    const reply = await fetch(`https://api.intercom.io/articles/${int_Response[i][_articleID]}`, CreateHeader("Intercom", "PUT", JSON.stringify({
-        body: ko_Response[ko_article][_articleBody].toString()
-    })));
-    const data = await reply.json();
-    LogInfo(JSON.stringify(data, null, 2) + '\n');
-    LogInfo(`"message":"Article Updated. Waiting..."`);
-    await setTimeout(function Waitfor() { }, 5000)
+  const reply = await fetch(
+    `https://api.intercom.io/articles/${int_Response[i][_articleID]}`,
+    CreateHeader(
+      "Intercom",
+      "PUT",
+      JSON.stringify({
+        body: ko_Response[ko_article][_articleBody].toString(),
+      })
+    )
+  );
+  const data = await reply.json();
+  LogInfo(JSON.stringify(data, null, 2) + "\n");
+  LogInfo(`"message":"Article Updated. Waiting..."`);
+  await setTimeout(function Waitfor() { }, 5000);
 }
-
 async function CreateArticle(ko_article, _articleTile) {
-    LogInfo(`"No article with matching title": "${ko_Response[ko_article][_articleTile]}", "Action":"Creating Article.."\n`);
-    const reply = await fetch('https://api.intercom.io/articles', CreateHeader("Intercom", "POST", JSON.stringify({
+  LogInfo(
+    `"No article with matching title": "${ko_Response[ko_article][_articleTile]}", "Action":"Creating Article.."\n`
+  );
+  const reply = await fetch(
+    "https://api.intercom.io/articles",
+    CreateHeader(
+      "Intercom",
+      "POST",
+      JSON.stringify({
         author_id: 7491322,
         title: ko_Response[ko_article][_articleTile].toString(),
         body: ko_Response[ko_article][_articleBody].toString(),
-        state: 'draft'
-    })));
-    const data = await reply.json();
-    LogInfo(JSON.stringify(data, null, 2) + '\n');
-    LogInfo(`"message":"Article Created. Waiting..."`);
-    await setTimeout(function Waitfor() { }, 3000)
+        state: "draft",
+      })
+    )
+  );
+  const data = await reply.json();
+  LogInfo(JSON.stringify(data, null, 2) + "\n");
+  LogInfo(`"message":"Article Created. Waiting..."`);
+  await setTimeout(function Waitfor() { }, 3000);
 }
-
 function CallAPI(headerType, requestMethod, apiUrl, ...apiArgs) {
-    return new Promise((resolve, reject) => {
-        if (apiArgs.length > 0) {
-            for (let i = 0; i < apiArgs.length; i++) {
-                apiUrl += apiArgs[i] + '&';
-            }
-            apiUrl = apiUrl.slice(0, -1);
-        }
-        const reqOptions = CreateHeader(headerType, requestMethod);
-        fetch(apiUrl, reqOptions)
-            .then(response => {
-                return response.json();
-            })
-
-            .then(data => {
-                //console.log(JSON.stringify(data, null, 2));
-                var apiItems = [];
-                var article;
-                if (data.data.length > 1) {
-                    data.data.forEach(obj => {
-                        if (headerType == "Knowledge_Owl") {
-                            maxPages = data.page_stats.total_pages;
-                            article = [obj.name, obj.current_version.en.text.replace(/\n/g, ''), obj.id];
-                            apiItems.push(article);
-                        }
-                        else {
-                            maxPages = data.pages.total_pages;
-                            article = [obj.title, obj.body, obj.id]
-                            apiItems.push(article);
-                        }
-                    });
-                }
-                else {
-                    if (headerType == "Knowledge_Owl") {
-                        article = [data.data.name, data.data.current_version.en.text.replace(/\n/g, ''), data.data.id];
-                        apiItems.push(article);
-                    }
-                    else {
-                        article = [data.title, data.body, data.id]
-                        apiItems.push(article);
-                    }
-                }
-                resolve(apiItems);
-            })
-            .catch(error => {
-                console.error('There has been a problem with your fetch operation:', error);
-                reject(error);
-            });
-    });
-}
-
-function CreateHeader(system, requestMethod, bodyValues = null) {
-    var requestOptions;
-    switch (system) {
-        case 'Knowledge_Owl':
-            requestOptions = {
-                method: requestMethod,
-                headers: {
-                    'Authorization': `Basic ${btoa(`${configFile.Knowledge_Owl.API_Key}:${configFile.Knowledge_Owl.Password}`)}`,
-                },
-            };
-            return requestOptions;
-
-        case 'Intercom':
-            requestOptions = {
-                method: requestMethod, headers: {
-                    'Authorization': `Bearer ${configFile.Intercom.Bearer_Token}`,
-                    'Accept': 'application/json',
-                    'Intercom-Version': '2.11',
-                    'Content-Type': 'application/json'
-                },
-                body: bodyValues,
-            };
-            return requestOptions;
+  return new Promise((resolve, reject) => {
+    if (apiArgs.length > 0) {
+      for (let i = 0; i < apiArgs.length; i++) {
+        apiUrl += apiArgs[i] + "&";
+      }
+      apiUrl = apiUrl.slice(0, -1);
     }
+    const reqOptions = CreateHeader(headerType, requestMethod);
+    fetch(apiUrl, reqOptions)
+      .then((response) => {
+        return response.json();
+      })
+
+      .then((data) => {
+        //console.log(JSON.stringify(data, null, 2));
+        var apiItems = [];
+        var article;
+        if (data.data.length > 1) {
+          data.data.forEach((obj) => {
+            if (headerType == "Knowledge_Owl") {
+              maxPages = data.page_stats.total_pages;
+              article = [
+                obj.name,
+                obj.current_version.en.text.replace(/\n/g, ""),
+                obj.id,
+              ];
+              apiItems.push(article);
+            } else {
+              maxPages = data.pages.total_pages;
+              article = [obj.title, obj.body, obj.id];
+              apiItems.push(article);
+            }
+          });
+        } else {
+          if (headerType == "Knowledge_Owl") {
+            article = [
+              data.data.name,
+              data.data.current_version.en.text.replace(/\n/g, ""),
+              data.data.id,
+            ];
+            apiItems.push(article);
+          } else {
+            article = [data.title, data.body, data.id];
+            apiItems.push(article);
+          }
+        }
+        resolve(apiItems);
+      })
+      .catch((error) => {
+        console.error(
+          "There has been a problem with your fetch operation:",
+          error
+        );
+        reject(error);
+      });
+  });
+}
+function CreateHeader(system, requestMethod, bodyValues = null) {
+  var requestOptions;
+  switch (system) {
+    case "Knowledge_Owl":
+      requestOptions = {
+        method: requestMethod,
+        headers: {
+          Authorization: `Basic ${btoa(
+            `${configFile.Knowledge_Owl.API_Key}:${configFile.Knowledge_Owl.Password}`
+          )}`,
+        },
+      };
+      return requestOptions;
+
+    case "Intercom":
+      requestOptions = {
+        method: requestMethod,
+        headers: {
+          Authorization: `Bearer ${configFile.Intercom.Bearer_Token}`,
+          Accept: "application/json",
+          "Intercom-Version": "2.11",
+          "Content-Type": "application/json",
+        },
+        body: bodyValues,
+      };
+      return requestOptions;
+  }
 }
 async function LogInfo(logContents) {
-    const output = await fs.appendFile(logFilePath, `"${GetDateTime()}":{${logContents}\n},`, (err) => { });
-    console.log(logContents);
+  const output = await fs.appendFile(
+    logFilePath,
+    `"${GetDateTime()}":{${logContents}\n},`,
+    (err) => { }
+  );
+  Output(logContents);
 }
-
 function GetDateTime() {
-    const currentDate = new Date();
-    //DD-MM-YY--HH-MM-SS
-    const now = `${currentDate.getDate()}-${currentDate.getMonth() + 1}-${currentDate.getFullYear()}--${currentDate.getHours()}-${currentDate.getMinutes()}-${currentDate.getSeconds()}`;
-    return now
+  const currentDate = new Date();
+  //DD-MM-YY--HH-MM-SS
+  const now = `${currentDate.getDate()}-${currentDate.getMonth() + 1
+    }-${currentDate.getFullYear()}--${currentDate.getHours()}-${currentDate.getMinutes()}-${currentDate.getSeconds()}`;
+  return now;
 }
-
-//Program_Switch(switchVal)
--- kbServer.js
+++ kbServer.js
@@ -1,87 +1,78 @@
-var http = require('http');
-var fs = require('fs');
-var os = require('os');
-var path = require('path');
-const { Program_Switch } = require('./Knowledge Owl Migration Tool.js');
+module.exports = { Output };
 
-if (!fs.existsSync('logs')) {
-  fs.mkdirSync('logs');
+var http = require("http");
+var fs = require("fs");
+var os = require("os");
+var path = require("path");
+const { Program_Switch } = require("./Knowledge Owl Migration Tool.js");
+
+if (!fs.existsSync("logs")) {
+  fs.mkdirSync("logs");
 }
 
-const logFilePath = path.join(path.join(__dirname, 'logs'), `log-${GetDateTime()}.json`);
+const logFilePath = path.join(
+  path.join(__dirname, "logs"),
+  `log-${GetDateTime()}.json`
+);
 
-var port = 5555;
+var port = Math.floor(Math.random() * (8000 - 5000 + 1)) + 5000;
 var log_to_file_queue = [];
 var log_to_server_queue = [];
 
 const localIP = getLocalIP();
-const originalConsoleLog = console.log;
-
-console.log = function (message) {
-  // Call the original console.log function
-  originalConsoleLog.apply(console, arguments);
-  log_to_server_queue.push(Array.from(arguments).join(' '));
-};
 
 function StartServer() {
   const server = http.createServer((req, res) => {
-    const clientIP = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    const clientIP = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
 
-    Output(`Requested URL: ${req.url} at: ${clientIP}`);
-
-    if (req.method === 'GET') {
-      const filePath = `.${req.url === '/' ? '/index.html' : req.url}`;
-      const contentType = getContentType(filePath);
-      serveStaticFile(filePath, contentType, res);
+    if (req.url !== "/events") {
+      Output(`Requested URL: ${req.url} at: ${clientIP}`);
     }
 
-    else if (req.method === 'POST') {
-      if (req.url === '/update&Create') {
+    if (req.method === "GET") {
+      const filePath = `.${req.url === "/" ? "/index.html" : req.url}`;
+      const contentType = getContentType(filePath);
+      serveStaticFile(filePath, contentType, res);
+    } else if (req.method === "POST") {
+      if (req.url === "/update&Create") {
         Output("Updating and Creating new articles");
-        Program_Switch(443);
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.write("some test text");
+        Program_Switch(1);
+        //Program_Switch(443);
         res.end();
-      }
-      else if (req.url === '/updateOnly') {
+      } else if (req.url === "/updateOnly") {
         Output("Updating all articles");
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.write("some test text");
         res.end();
-      }
-      else if (req.url === '/createOnly') {
+      } else if (req.url === "/createOnly") {
         Output("Creating new articles");
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.write("some test text");
         res.end();
-      }
-      else if (req.url.includes('/updateSpecific')) {
-        const reqParams = req.url.split('/');
+      } else if (req.url.includes("/updateSpecific")) {
+        const reqParams = req.url.split("/");
         const inputValue = reqParams[2];
         Output(`Updating article ${inputValue}`);
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.write(`some test text - ${inputValue}`);
         res.end();
-      }
-      else if (req.url.includes('/removeSpecific')) {
-        const reqParams = req.url.split('/');
+      } else if (req.url.includes("/createSpecific")) {
+        const reqParams = req.url.split("/");
         const inputValue = reqParams[2];
-
+        Output(`Creating article ${inputValue}`);
+        res.end();
+      } else if (req.url.includes("/removeSpecific")) {
+        const reqParams = req.url.split("/");
+        const inputValue = reqParams[2];
         Output(`Removing article ${inputValue}`);
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.write(`Removing article - ${inputValue}`);
         res.end();
-      }
-      else if (req.url === '/events') {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.write(`${log_to_server_queue[0]}\n`);
-        res.end();
-        log_to_server_queue.shift();
-      }
-      else {
+      } else if (req.url === "/events") {
+        if (log_to_server_queue.length > 0) {
+          res.writeHead(200, { "Content-Type": "text/plain" });
+          res.write(log_to_server_queue.shift() + '\n');
+          res.end();
+        }
+        else {
+          res.end();
+        }
+      } else {
         // Invalid POST request
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Invalid POST request');
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Invalid POST request");
       }
     }
   });
@@ -91,14 +82,13 @@ function StartServer() {
     //Output(`Server on http://127.0.0.1:${port}`);
   });
 
-  server.on('error', (e) => {
-    if (e.code === 'EADDRINUSE') {
+  server.on("error", (e) => {
+    if (e.code === "EADDRINUSE") {
       let randomValue = Math.floor(Math.random() * (8000 - 5000 + 1)) + 5000;
       Output(`Port ${port} is already in use. Changing port to ${randomValue}`);
       port = randomValue;
       StartServer();
-    }
-    else {
+    } else {
       console.log(e.code);
     }
   });
@@ -109,43 +99,43 @@ function getLocalIP() {
   for (let name in interfaces) {
     for (let iface of interfaces[name]) {
       // Skip over non-IPv4 and internal (i.e., 127.0.0.1) addresses
-      if (iface.family === 'IPv4' && !iface.internal) {
+      if (iface.family === "IPv4" && !iface.internal) {
         return iface.address;
       }
     }
   }
-  return '127.0.0.1'; // Fallback to localhost if no IP is found
+  return "127.0.0.1"; // Fallback to localhost if no IP is found
 }
 
 // Function to get the content type based on the file extension
 function getContentType(filePath) {
   const extname = path.extname(filePath).toLowerCase();
   const mimeTypes = {
-    '.html': 'text/html',
-    '.css': 'text/css',
-    '.js': 'application/javascript',
-    '.mp4': 'video/mp4',
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "application/javascript",
+    ".mp4": "video/mp4",
   };
 
-  return mimeTypes[extname] || 'application/octet-stream';
+  return mimeTypes[extname] || "application/octet-stream";
 }
 
 // Function to serve static files
 function serveStaticFile(filePath, contentType, res) {
   fs.readFile(filePath, (err, data) => {
     if (err) {
-      if (err.code === 'ENOENT') {
+      if (err.code === "ENOENT") {
         // File not found
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end('<h1>404 Not Found</h1>');
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>404 Not Found</h1>");
       } else {
         // Other read errors
-        res.writeHead(500, { 'Content-Type': 'text/html' });
+        res.writeHead(500, { "Content-Type": "text/html" });
         res.end(`<h1>500 Internal Server Error</h1><p>${err.message}</p>`);
       }
     } else {
       // File read successfully, serve it
-      res.writeHead(200, { 'Content-Type': contentType });
+      res.writeHead(200, { "Content-Type": contentType });
       res.end(data);
     }
   });
@@ -154,13 +144,18 @@ function serveStaticFile(filePath, contentType, res) {
 function Output(outputData) {
   console.log(outputData.toString());
   log_to_file_queue.push(outputData.toString());
+  log_to_server_queue.push(outputData);
 }
 
 async function LogData() {
   while (true) {
-    await new Promise(resolve => setTimeout(resolve, 1000));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     if (log_to_file_queue.length > 0) {
-      await fs.appendFile(logFilePath, `"${GetDateTime()}":${JSON.stringify(log_to_file_queue[0], null, 2)}\n`, (err) => { });
+      await fs.appendFile(
+        logFilePath,
+        `"${GetDateTime()}":${JSON.stringify(log_to_file_queue[0], null, 2)}\n`,
+        (err) => { }
+      );
       log_to_file_queue.shift();
     }
   }
@@ -169,10 +164,10 @@ async function LogData() {
 function GetDateTime() {
   const currentDate = new Date();
   //DD-MM-YY--HH-MM-SS
-  const now = `${currentDate.getDate()}-${currentDate.getMonth() + 1}-${currentDate.getFullYear()}--${currentDate.getHours()}-${currentDate.getMinutes()}-${currentDate.getSeconds()}`;
-  return now
+  const now = `${currentDate.getDate()}-${currentDate.getMonth() + 1
+    }-${currentDate.getFullYear()}--${currentDate.getHours()}-${currentDate.getMinutes()}-${currentDate.getSeconds()}`;
+  return now;
 }
-
 
 LogData();
 StartServer();
