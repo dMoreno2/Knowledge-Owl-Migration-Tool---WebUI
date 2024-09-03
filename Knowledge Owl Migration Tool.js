2744d6c654c270761da44ca610b91563b0cc0e06
dMoreno2 <d.edwardmoreno@gmail.com>
2024-09-03 15:47:11 +1000

    Added extra varrible for articles url/slug; Added the URL or Slug of article into ko/intercom articles matrix; Created ReplaceArticleLinks() function to replace links in the articles to fix inter-referencing; Disabled the ProcessArticles() function for testing;

--- Knowledge Owl Migration Tool.js
+++ Knowledge Owl Migration Tool.js
@@ -17,9 +17,11 @@ var pageCount = 1;
 var ko_Response = [];
 var int_Response = [];
 
-let _articleTile = 0; /// ko_Response [0] - Article Title - int_Response [0]
+let _articleTitle = 0; /// ko_Response [0] - Article Title - int_Response [0]
 let _articleBody = 1; /// ko_Response [1] - Article Body  - int_Response [1]
 let _articleID = 2;   /// ko_Response [2] - Article ID    - int_Response [2]
+let _articleSlug = 3; /// ko_Response [3] - Article Slug  - int Response [3]
+
 try {
     const data = fs.readFileSync("config.json", 'utf8');
     configFile = JSON.parse(data);
@@ -60,21 +62,23 @@ async function Update_And_Create_Articles() {
     await Get_KO_Articles();
     await Get_Intercom_Articles();
     //ProcessArticles(create, update)
-    ProcessArticles(true, true);
+    //ProcessArticles(true, true);
     LogInfo("Final:Processing Complete");
 }
 async function UpdateArticlesOnly() {
     await Get_KO_Articles();
     await Get_Intercom_Articles();
+    ReplaceArticleLinks();
     //ProcessArticles(create, update)
-    ProcessArticles(false, true);
+    //ProcessArticles(false, true);
+
     LogInfo("Final:Processing Complete");
 }
 async function CreatArticlesOnly() {
     await Get_KO_Articles();
     await Get_Intercom_Articles();
     //ProcessArticles(create, update)
-    ProcessArticles(true, false);    
+    //ProcessArticles(true, false);    
     LogInfo("Final:Processing Complete");
 }
 async function Get_KO_Articles() {
@@ -85,7 +89,7 @@ async function Get_KO_Articles() {
         const resp = await CallAPI("Knowledge_Owl", "GET", `${configFile.Knowledge_Owl.Search_URL}?`, `project_id=${configFile.Knowledge_Owl.Project_ID}`, "status=published", `page=${pageCount}`);
         for (let index = 0; index < resp.length; index++) {
             ko_Response.push(resp[index])
-            LogInfo(`"ko-name": "${resp[index][_articleTile]}"`);
+            LogInfo(`"ko-name": "${resp[index][_articleTitle]}"`);
         }
         setTimeout(function Waitfor() {
             LogInfo(`"message":"ko-reply ${pageCount}"`);
@@ -102,7 +106,7 @@ async function Get_Intercom_Articles() {
         const resp = await CallAPI("Intercom", "GET", configFile.Intercom.Create_URL, '?page=' + pageCount);
         for (let index = 0; index < resp.length; index++) {
             int_Response.push(resp[index])
-            LogInfo(`"int-name": "${resp[index][_articleTile]}"`);
+            LogInfo(`"int-name": "${resp[index][_articleTitle]}"`);
         }
         setTimeout(function Waitfor() {
             LogInfo(`"message":"Int-reply ${pageCount}"`);
@@ -115,11 +119,11 @@ async function ProcessArticles(create, update) {
     for (let ko_article = 0; ko_article < ko_Response.length; ko_article++) {
         var exists = false;
         for (let i = 0; i < int_Response.length; i++) {
-            LogInfo(`"Checking for article": "${ko_Response[ko_article][_articleTile]}..."`);
-            if (ko_Response[ko_article][_articleTile].replace(/\s+/g, '') === int_Response[i][_articleTile].replace(/\s+/g, '')) {
+            LogInfo(`"Checking for article": "${ko_Response[ko_article][_articleTitle]}..."`);
+            if (ko_Response[ko_article][_articleTitle].replace(/\s+/g, '') === int_Response[i][_articleTitle].replace(/\s+/g, '')) {
                 if (update === true) {
-                    LogInfo(`"Updating Article": "${ko_Response[ko_article][_articleTile]}"\n`);
-                    await UpdateArticle(ko_article, _articleTile, i);
+                    LogInfo(`"Updating Article": "${ko_Response[ko_article][_articleTitle]}"\n`);
+                    await UpdateArticle(ko_article, _articleTitle, i);
                     break;
                 }
                 exists = true;
@@ -129,8 +133,8 @@ async function ProcessArticles(create, update) {
             }
         }
         if (exists == false && create === true) {
-            LogInfo(`Creating article: ${ko_Response[ko_article][_articleTile]}...`);
-            await CreateArticle(ko_article, _articleTile);
+            LogInfo(`Creating article: ${ko_Response[ko_article][_articleTitle]}...`);
+            await CreateArticle(ko_article, _articleTitle);
         }
         else {
             exists = false;
@@ -138,7 +142,7 @@ async function ProcessArticles(create, update) {
         iterationCount++;
     }
 }
-async function UpdateArticle(ko_article, _articleTile, i) {
+async function UpdateArticle(ko_article, _articleTitle, i) {
     const reply = await fetch(`https://api.intercom.io/articles/${int_Response[i][_articleID]}`, CreateHeader("Intercom", "PUT", JSON.stringify({
         body: ko_Response[ko_article][_articleBody].toString()
     })));
@@ -147,11 +151,11 @@ async function UpdateArticle(ko_article, _articleTile, i) {
     LogInfo(`"message":"Article Updated. Waiting..."`);
     await setTimeout(function Waitfor() { }, 5000)
 }
-async function CreateArticle(ko_article, _articleTile) {
-    LogInfo(`"No article with matching title": "${ko_Response[ko_article][_articleTile]}", "Action":"Creating Article.."\n`);
+async function CreateArticle(ko_article, _articleTitle) {
+    LogInfo(`"No article with matching title": "${ko_Response[ko_article][_articleTitle]}", "Action":"Creating Article.."\n`);
     const reply = await fetch('https://api.intercom.io/articles', CreateHeader("Intercom", "POST", JSON.stringify({
         author_id: 7491322,
-        title: ko_Response[ko_article][_articleTile].toString(),
+        title: ko_Response[ko_article][_articleTitle].toString(),
         body: ko_Response[ko_article][_articleBody].toString(),
         state: 'draft'
     })));
@@ -159,6 +163,19 @@ async function CreateArticle(ko_article, _articleTile) {
     LogInfo(JSON.stringify(data, null, 2) + '\n');
     LogInfo(`"message":"Article Created. Waiting..."`);
     await setTimeout(function Waitfor() { }, 3000)
+}
+function ReplaceArticleLinks() {
+    for (let ko_article = 0; ko_article < ko_Response.length; ko_article++) { //body
+        for (let b = 0; b < ko_Response.length; b++) {  //slug
+            if (ko_Response[ko_article][_articleBody].includes(ko_Response[b][_articleSlug])) {
+                for (let index = 0; index < int_Response.length; index++) {
+                    if (ko_Response[b][_articleTitle].replace(/\s+/g, '') === int_Response[index][_articleTitle].replace(/\s+/g, '')) {
+                        ko_Response[ko_article][_articleBody] = ko_Response[ko_article][_articleBody].replace(new RegExp(`href=\\"([^"]*${ko_Response[b][_articleSlug]}[^"]*)\\"`, 'g'),`href="${int_Response[index][_articleSlug]}"`);
+                    }
+                }
+            }
+        }
+    }
 }
 function CallAPI(headerType, requestMethod, apiUrl, ...apiArgs) {
     return new Promise((resolve, reject) => {
@@ -182,23 +199,23 @@ function CallAPI(headerType, requestMethod, apiUrl, ...apiArgs) {
                     data.data.forEach(obj => {
                         if (headerType == "Knowledge_Owl") {
                             maxPages = data.page_stats.total_pages;
-                            article = [obj.name, obj.current_version.en.text.replace(/\n/g, ''), obj.id];
+                            article = [obj.name, obj.current_version.en.text.replace(/\n/g, ''), obj.id, obj.url_hash];
                             apiItems.push(article);
                         }
                         else {
                             maxPages = data.pages.total_pages;
-                            article = [obj.title, obj.body, obj.id]
+                            article = [obj.title, obj.body, obj.id, obj.url]
                             apiItems.push(article);
                         }
                     });
                 }
                 else {
                     if (headerType == "Knowledge_Owl") {
-                        article = [data.data.name, data.data.current_version.en.text.replace(/\n/g, ''), data.data.id];
+                        article = [data.data.name, data.data.current_version.en.text.replace(/\n/g, ''), data.data.id, data.data.url_hash];
                         apiItems.push(article);
                     }
                     else {
-                        article = [data.title, data.body, data.id]
+                        article = [data.title, data.body, data.id, data.url]
                         apiItems.push(article);
                     }
                 }
